327a3ecec874d862158889d11b05c6fded66a9cb
sidrawazir23-dot <sidrawazir23@gmail.com>
2025-11-03 23:40:11 -0800

    add js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+<script>
+    function register(){
+        alert("Redirecting to registration page...");
+    }
+    function book(){
+        alert("Redirecting to booking page...");
+    }
+    document.getElementById("getstartedBtn").
+    addEventListener("click",function(){
+        alert("let's get you started on your Glam Journey!");
+    });
+   </script>
